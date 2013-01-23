996a2869cb3c342c7f73d4dfbacabe501b6fd5a5
cheptsov <andrey.cheptsov@jetbrains.com>
2013-01-23 19:39:42 +0400

    Tests for spring MVC

---
+++ spring/SpringAppMVC/src/test/java/com/springapp/mvc/SpringAppMVCTests.java
@@ -0,0 +1,43 @@
+package com.springapp.mvc;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration("classpath:spring-config.xml")
+public class SpringAppMVCTests {
+    private MockMvc mockMvc;
+
+    @SuppressWarnings("SpringJavaAutowiringInspection")
+    @Autowired
+    protected WebApplicationContext wac;
+
+    @Autowired
+    protected HelloController helloController;
+
+    @Before
+    public void setup() {
+        this.mockMvc = webAppContextSetup(this.wac).build();
+    }
+
+    @Test
+    public void simple() throws Exception {
+        standaloneSetup(helloController).build()
+                .perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("hello"));
+    }
+}
