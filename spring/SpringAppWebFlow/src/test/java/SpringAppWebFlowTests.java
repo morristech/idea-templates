b79238329f958f37e2f4d6e1c19b84a4d07a7381
cheptsov <andrey.cheptsov@jetbrains.com>
2013-01-24 10:03:17 +0400

    Unit tests for MVC and WebFlow

---
+++ spring/SpringAppWebFlow/src/test/java/SpringAppWebFlowTests.java
@@ -0,0 +1,22 @@
+import org.springframework.webflow.config.FlowDefinitionResource;
+import org.springframework.webflow.config.FlowDefinitionResourceFactory;
+import org.springframework.webflow.test.MockExternalContext;
+import org.springframework.webflow.test.MockFlowBuilderContext;
+import org.springframework.webflow.test.execution.AbstractXmlFlowExecutionTests;
+
+public class SpringAppWebFlowTests extends AbstractXmlFlowExecutionTests {
+    @Override
+    protected void configureFlowBuilderContext(MockFlowBuilderContext builderContext) {
+    }
+
+    @Override
+    protected FlowDefinitionResource getResource(FlowDefinitionResourceFactory flowDefinitionResourceFactory) {
+        return flowDefinitionResourceFactory.createFileResource("src/main/webapp/WEB-INF/flows/hello-flow.xml");
+    }
+
+    public void testHelloFlow() {
+        MockExternalContext context = new MockExternalContext();
+        startFlow(context);
+        assertCurrentStateEquals("hello");
+    }
+}
