6e5e2fc631f0f95838f6064f94cd13812ba5dd1b
cheptsov <andrey.cheptsov@jetbrains.com>
2013-01-30 18:43:54 +0400

    Bugfix

---
+++ javaee/JavaEEAppCDIJSF/src/main/java/com/javaeeapp/jsf/HelloBean.java
@@ -0,0 +1,41 @@
+package com.javaeeapp.jsf;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.ResourceBundle;
+
+@RequestScoped
+@Named
+public class HelloBean {
+    @Inject
+    private Hello hello;
+
+    @NotNull()
+    private String name;
+
+    private String result;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public String sayHello() {
+        result = hello.sayHello(name);
+        return "success";
+    }
+}
