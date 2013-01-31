e050f0f9af4c2fc557543042e4d73e492df81c55
cheptsov <andrey.cheptsov@jetbrains.com>
2013-01-31 16:21:58 +0400

    Run configuration, description, etc.

--- spring/SpringAppIntegration/src/test/java/com/springapp/integration/AppTests.java
+++ spring/SpringAppIntegration/src/test/java/com/springapp/integration/AppTests.java
@@ -13,7 +13,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:spring-config.xml")
-public class SpringAppIntegrationTests {
+public class AppTests {
     @Qualifier("inputChannel")
     @Autowired
     private MessageChannel inputChannel;
